90c92c3d533b755230c7aa5fc3b4053abd046a09
mrunu002-codes <mrunalrote4@gmail.com>
2026-06-25 12:56:34 +0530

    Intial commit

---
+++ src/Mutlithreading/Synconisation/ABCPrinting.java
@@ -0,0 +1,58 @@
+package Mutlithreading.Synconisation;
+
+
+class ABCPrinter {
+    private int turn=0;
+
+    public synchronized void printA() {
+        for(int i=0;i<5;i++){
+            while(turn!=0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+                System.out.println("A");
+                turn=1;
+                notifyAll();
+
+        }
+    }
+    public synchronized void printB(){
+        for(int i=0;i<5;i++){
+            while(turn!=1){
+                try{
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("B");
+            turn=2;
+            notifyAll();
+        }
+    }
+    public synchronized void printC(){
+        for(int i=0;i<5;i++){
+            while(turn!=2){
+                try{
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("C");
+            turn=0;
+            notifyAll();
+        }
+    }
+}
+public class ABCPrinting {
+    public static void main(String[] args) {
+        ABCPrinter abcPrinter=new ABCPrinter();
+        new Thread(abcPrinter::printA).start();
+        new Thread(abcPrinter::printB).start();
+        new Thread(abcPrinter::printC).start();
+    }
+}
---
+++ src/Mutlithreading/Synconisation/OddEvenPrinter.java
@@ -0,0 +1,51 @@
+package Mutlithreading.Synconisation;
+
+class Printer{
+    int num=1;
+    synchronized void printEven() throws InterruptedException {
+        while(num<=10){
+            while(num%2==0){
+                wait();
+            }
+            System.out.println(num);
+            num++;
+            notify();
+        }
+    }
+    synchronized void printOdd() throws InterruptedException{
+        while(num<=10){
+            while(num%2==1){
+                wait();
+            }
+            System.out.println(num);
+            num++;
+            notify();
+        }
+    }
+}
+public class OddEvenPrinter {
+    public static void main(String[] args) {
+
+        Printer printer=new Printer();
+        Thread t1 =new Thread(
+                () -> {
+                    try{
+                        printer.printEven();
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+        );
+        Thread t2= new Thread(
+                () -> {
+                    try{
+                        printer.printOdd();
+                    }catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+        );
+        t1.start();
+        t2.start();
+    }
+}
